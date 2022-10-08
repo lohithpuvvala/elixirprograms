b47fe292d44c59960b225e618988b0c305baa376
lohithpuvvala <99991314+lohithpuvvala@users.noreply.github.com>
2022-10-08 12:05:23 +0530

    Update test.ex

--- test.ex
+++ test.ex
@@ -1,6 +1,14 @@
 defmodule M do
-  def enum do
-    R = Enum.map([1,2,3],fn x -> x*2 end)
-    IO.puts "Result : #{R}"
+  def main do
+    matrix_of_sum = [
+
+    ]
+    valid_sum(matrix_of_sum)
+  end
+
+  def valid_sum(matrix_of_sum) do
+    list = List.flatten(matrix_of_sum)
+    list
+    |> Enum.filter(fn(x) -> is_integer(x) == true end)
   end
 end
